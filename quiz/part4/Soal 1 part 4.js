ba3689c6d4681caea7021a01e0e37f2a554b8143
fatihbadasss <fatihputra23@gmail.com>
2025-05-04 11:18:08 +0700

    part 4

---
+++ quiz/part4/Soal 1 part 4.js
@@ -0,0 +1,15 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya 
+function shoutOut() {
+    return "Halo function!";
+}
+
+console.log(shoutOut());
+
+
+// ------------------------------------
+console.log('')
+// ------------------------------------
